7dfb574c374da8ebcf848ae44b40c084a553d47e
AdamM1080 <adam.maclean@my.jcu.edu.au>
2025-10-05 07:26:37 +1000

    Add randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,25 @@
+# What did you see on line 1?
+# What was the smallest number you could have seen, what was the largest?
+
+# Smallest: 5
+# Largest: 19
+
+# What did you see on line 2?
+# What was the smallest number you could have seen, what was the largest?
+# Could line 2 have produced a 4?
+
+# Smallest: 3
+# Largest: 9
+# No it could not have produced a 4.
+
+# What did you see on line 3?
+# What was the smallest number you could have seen, what was the largest?
+
+# Smallest: 2.5
+# Largest: 5.49999999 recurring?
+
+# Write code, not a comment, to produce a random number between 1 and 100 inclusive.
+
+from random import randint
+
+print(randint(1, 100))
