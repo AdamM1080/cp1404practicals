93d274798ee9015419de60204316c101392bb75a
AdamM1080 <adam.maclean@my.jcu.edu.au>
2025-11-05 15:02:12 +1000

    Complete second part of More Guitars Exercise

--- prac_07/guitar.py
+++ prac_07/guitar.py
@@ -7,18 +7,23 @@ Actual time: 15 minutes
 
 class Guitar:
     def __init__(self, name="", year=0, cost=0):
+        """Initialize a Guitar object."""
         self.name = name
         self.year = int(year)
         self.cost = float(cost)
 
     def __str__(self):
+        """Return string representation of Guitar"""
         return f"{self.name} ({self.year}) : ${self.cost:.2f}"
 
     def get_age(self):
+        """Return age of Guitar"""
         return 2025 - self.year
 
     def is_vintage(self):
+        """Return True if Guitar is vintage"""
         return self.get_age() > 50
 
     def __lt__(self, other):
+        """Return if age of guitar is less than other guitar"""
         return self.year < other.year
--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -1,40 +1,65 @@
-""""""
+"""
+CP1404/CP5632 Practical - My Guitars!
+Estimated time: 30 minutes
+Actual time: An hour or two
+"""
+
 from prac_07.guitar import Guitar
 
 FILENAME = "guitars.csv"
 
+
 def main():
-    """My Guitars! - Display guitars entered by user"""
-    guitars = load_guitar_data(FILENAME)
+    """My Guitars! - Add and display guitars entered by user"""
+    guitars = load_guitars(FILENAME)
     guitars.sort()
     print("My Guitars!")
-    name = input("Name: ")
-    while name != "":
-        year = int(input("Year: "))
-        cost = float(input("Cost: "))
-        guitar = Guitar(name, year, cost)
-        print(f"{guitar} added")
-        name = input("Name: ")
-    display_guitar_data(guitars)
+    get_new_guitars(guitars)
+    display_guitars(guitars)
+    save_guitars(FILENAME, guitars)
 
 
-def load_guitar_data(filename):
+def load_guitars(filename):
+    """Load guitars from a CSV file"""
     guitars = []
-    with open(FILENAME, "r", encoding="utf-8-sig") as in_file:
+    with open(filename, "r", encoding="utf-8-sig") as in_file:
         for line in in_file:
             part = line.strip().split(",")
             name, year, cost = part[0], part[1], part[2]
             guitars.append(Guitar(name, year, cost))
+    in_file.close()
     return guitars
 
-def display_guitar_data(guitars):
+
+def get_new_guitars(guitars):
+    """Get new guitars from user input"""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        new_guitar = Guitar(name, year, cost)
+        guitars.append(new_guitar)
+        guitars.sort()
+        print(f"{new_guitar} added")
+        name = input("Name: ")
+
+
+def display_guitars(guitars):
     """Display guitars entered by user"""
     print("These are my guitars:")
     max_name_length = max(len(g.name) for g in guitars)
     for i, guitar in enumerate(guitars, 1):
         vintage_status = " (vintage)" if guitar.is_vintage() else ""
-        print(f"Guitar {i}: {guitar.name:{max_name_length}} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_status}")
+        print(
+            f"Guitar {i}: {guitar.name:{max_name_length}} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_status}")
 
+
+def save_guitars(filename, guitars):
+    """Save guitars entered by user to file"""
+    with open(filename, "w", encoding="utf-8") as out_file:
+        for guitar in guitars:
+            print(f"{guitar.name},{guitar.year},{guitar.cost:.2f}", file=out_file)
+    out_file.close()
 
 
 main()
